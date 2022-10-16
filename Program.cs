bd737bfcb82ee63799321236c799e787e946298e
KyryloStroiev <stroievkyrylo@gmail.com>
2022-10-16 21:52:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = new[] { 1, 2, 3 };
+
+AddX(arr, 5);
+
+
+
+foreach (var item in arr)
+
+{
+
+    Console.Write(item);
+
+}
+
+
+
+static void AddX(int[] arr, int x)
+
+{
+
+    for (var i = 0; i < arr.Length; ++i)
+
+    {
+
+        arr[i] += x;
+
+    }
+
+}
